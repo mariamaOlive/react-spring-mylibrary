e4846f67db9dcad3d15ba4e0f27e6372266d8815
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2026-04-27 20:40:35 -0300

    SpringBoot - Clean unused import files

--- easylibrary/src/main/java/com/mariama/easylibrary/repository/BookRepository.java
+++ easylibrary/src/main/java/com/mariama/easylibrary/repository/BookRepository.java
@@ -3,7 +3,6 @@ package com.mariama.easylibrary.repository;
 import com.mariama.easylibrary.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- easylibrary/src/main/java/com/mariama/easylibrary/service/impl/BookServiceImpl.java
+++ easylibrary/src/main/java/com/mariama/easylibrary/service/impl/BookServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import com.mariama.easylibrary.repository.BookRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
